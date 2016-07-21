88aa11f7fb972bf34676ecd1f74ad1cb762dd77c
Chelsea DuFour <chelseadufour@gmail.com>
2016-07-21 09:28:17 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+
+$(function (){
+  $.get('https://www.reddit.com/r/aww.json', function(awwData) {
+
+    console.log(awwData);
+    console.log(awwData.data.children[i]);
+    for(var i=0; i<10; i++){
+      var title = awwData.data.children[i].data.title;
+      var author = awwData.data.children[i].data.author;
+      var thumbnail = awwData.data.children[i].data.thumbnail;
+
+      console.log(title);
+      console.log(author);
+      console.log(thumbnail);
+
+      <'div'>.append(<'img'>);
+      <'img:last'>.attr('src', thumbnail);
+      <'div:last'>.append(title);
+      <'div:last'>.append(author);
+      <'div:last'>.append(thumbnail);
+
+
+    });
+  });
+};
+
+
+
+
+
+
+
+
+
+
+//slack Karen lab info
+
+
+
+
+
+
+});
